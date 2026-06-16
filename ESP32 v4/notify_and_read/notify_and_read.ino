1e3c286106edf9f165fc436c2780c359f71d9d14
choeyujeong911 <yujeongchoe20@gmail.com>
2026-06-16 13:28:44 +0900

    LED가 아닌 OLED 디스플레이에 값이 출력되도록 수정

--- ESP32 v4/notify_and_read/notify_and_read.ino
+++ ESP32 v4/notify_and_read/notify_and_read.ino
@@ -1,3 +1,8 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -5,20 +10,50 @@
 
 #define SVC_UUID      "a0b82d8f-d5a9-42ae-93dc-698df59ea89b"
 #define BTN_CHA_UUID  "5cb3ff02-3a98-4d6e-a366-d29c3d0249cc"
-#define LED_CHA_UUID  "32c86189-2864-4606-8010-8a267c12c9d8"
+#define WRITE_CHA_UUID "32c86189-2864-4606-8010-8a267c12c9d8"
 
-#define BTN_PIN  34
-#define LED_PIN  23
+#define BTN_PIN 34
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 BLEServer* server;
 BLECharacteristic* btnCharacteristic;
-BLECharacteristic* ledCharacteristic;
+BLECharacteristic* writeCharacteristic;
 
 int lastButtonState = -1;
 bool wasConnected = false;
 
+String notifyState = "OFF";
+String writeState = "OFF";
+
+void drawScreen() {
+  display.clearDisplay();
+
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  display.setTextWrap(false);
+
+  display.setCursor(5, 12);
+  display.print("Notify:");
+
+  display.setCursor(89, 12);
+  display.print(notifyState);
+
+  display.setCursor(17, 39);
+  display.print("Write:");
+
+  display.setCursor(89, 39);
+  display.print(writeState);
+
+  display.display();
+}
+
 // 앱에서 값을 WRITE했을 때 실행됨
-class LedWriteCallback : public BLECharacteristicCallbacks {
+class WriteCallback : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic* characteristic) {
     String value = characteristic->getValue();
 
@@ -27,14 +62,17 @@ class LedWriteCallback : public BLECharacteristicCallbacks {
       Serial.println(value);
 
       if (value == "1") {
-        digitalWrite(LED_PIN, HIGH);
-        Serial.println("LED_PIN: HIGH");
+        writeState = "ON";
+        Serial.println("Write: ON");
       } else if (value == "0") {
-        digitalWrite(LED_PIN, LOW);
-        Serial.println("LED_PIN: LOW");
+        writeState = "OFF";
+        Serial.println("Write: OFF");
       } else {
         Serial.println("알 수 없는 값");
+        return;
       }
+
+      drawScreen();
     }
   }
 };
@@ -43,9 +81,17 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(BTN_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
 
   Serial.println("START");
+
+  Wire.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("SSD1306 init failed");
+    while (true);
+  }
+
+  drawScreen();
 
   BLEDevice::init("ESP32_BleTest");
 
@@ -53,7 +99,7 @@ void setup() {
 
   BLEService* bleService = server->createService(SVC_UUID);
 
-  // 기존: ESP32 → 앱 송신용 READ + NOTIFY
+  // ESP32 → 앱 송신용 READ + NOTIFY
   btnCharacteristic = bleService->createCharacteristic(
     BTN_CHA_UUID,
     BLECharacteristic::PROPERTY_READ |
@@ -63,14 +109,14 @@ void setup() {
   btnCharacteristic->addDescriptor(new BLE2902());
   btnCharacteristic->setValue("OFF");
 
-  // 추가: 앱 → ESP32 수신용 WRITE
-  ledCharacteristic = bleService->createCharacteristic(
-    LED_CHA_UUID,
+  // 앱 → ESP32 수신용 WRITE
+  writeCharacteristic = bleService->createCharacteristic(
+    WRITE_CHA_UUID,
     BLECharacteristic::PROPERTY_WRITE
   );
 
-  ledCharacteristic->setCallbacks(new LedWriteCallback());
-  ledCharacteristic->setValue("0");
+  writeCharacteristic->setCallbacks(new WriteCallback());
+  writeCharacteristic->setValue("0");
 
   bleService->start();
 
@@ -79,36 +125,41 @@ void setup() {
   advertising->setScanResponse(true);
   advertising->start();
 
-  Serial.println("스위치 테스트 BLE 서비스 준비 완료");
+  Serial.println("BLE service ready");
 }
 
 void loop() {
   bool connected = server->getConnectedCount() > 0;
 
-  if (connected) {
-    int currentButtonState = digitalRead(BTN_PIN);
+  int currentButtonState = digitalRead(BTN_PIN);
 
-    if (currentButtonState != lastButtonState) {
-      if (currentButtonState == HIGH) {
-        btnCharacteristic->setValue("ON");
+  if (currentButtonState != lastButtonState) {
+    if (currentButtonState == HIGH) {
+      notifyState = "ON";
+      btnCharacteristic->setValue("ON");
+
+      if (connected) {
         btnCharacteristic->notify();
-
-        Serial.println("Switch: ON");
-      } else {
-        btnCharacteristic->setValue("OFF");
-        btnCharacteristic->notify();
-
-        Serial.println("Switch: OFF");
       }
 
-      lastButtonState = currentButtonState;
+      Serial.println("Notify: ON");
+    } else {
+      notifyState = "OFF";
+      btnCharacteristic->setValue("OFF");
+
+      if (connected) {
+        btnCharacteristic->notify();
+      }
+
+      Serial.println("Notify: OFF");
     }
+
+    drawScreen();
+    lastButtonState = currentButtonState;
   }
 
   if (!connected && wasConnected) {
-    Serial.println("연결 끊김");
-    lastButtonState = -1;
-
+    Serial.println("Disconnected");
     BLEDevice::startAdvertising();
   }
 
